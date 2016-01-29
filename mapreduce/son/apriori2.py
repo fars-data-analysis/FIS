bc1ac09c5945c3710dcf6149096eeffaebe59ef0
djbb7 <daniel.bruzual@gmail.com>
2016-01-29 17:04:13 +0100

    Remove faulty preserveMapping param

--- mapreduce/son/apriori2.py
+++ mapreduce/son/apriori2.py
@@ -31,38 +31,38 @@ def findFrequentItemsets(input, output, numPartitions, s, sc):
 
     count = data.count()
 
-    threshold = s*count
+    threshold = int(s)
 
     res = {}
 
     #split string baskets into lists of items
     #Result: ([items])
-    res[0] = baskets = data.map(lambda line: sorted([int(y) for y in line.strip().split(' ')]))
+    res[0] = baskets = data.map(lambda line: sorted([int(y) for y in line.strip().split(' ')]), True)
 
     #key each basket by partition id, to ensure work is done locally
     #break down transactions into single items, with an id referring to the basket
     #Result: ((partId, basketId), item)
-    res[1] = expandedBaskets = baskets.mapPartitionsWithIndex(getBreakUp())
+    res[1] = expandedBaskets = baskets.mapPartitionsWithIndex(getBreakUp()).persist()
 
     #classic word count
-    #Result: ((partId, tuple([items])), count)
-    res[2] = candidates_1 = expandedBaskets.map(lambda ((partId,basketId),item): ((partId, tuple([item])),1), True).reduceByKey(lambda v1, v2: v1+v2)
+    #Result: ((partId, tuple([item])), count)
+    res[2] = candidates_1 = expandedBaskets.map(lambda ((partId,basketId),item): ((partId, tuple([item])),1)).reduceByKey(lambda v1, v2: v1+v2)
 
     #Add up counts and filter with threshold
     #Result: ((partId, tuple([items])), count)
     res[3] = frequent_1 = candidates_1.filter(lambda c: c[1]>=threshold/numPartitions)
 
     #Remove unfrequent items from baskets
-    #Result: ((partId, basketId), tuple([items]
-    res[4] = expandedBaskets = expandedBaskets.map(lambda ((p, b),i): ((p,tuple([i])), b), True).leftOuterJoin(frequent_1, numPartitions).filter(lambda ((p, i), (b,count)): count!=None).map(lambda ((p,i),(b,count)): ((p,b),i), True)
+    #Result: ((partId, basketId), tuple([items])
+    res[4] = expandedBaskets = expandedBaskets.map(lambda ((p, b),i): ((p,tuple([i])), b)).join(frequent_1).map(lambda ((p,i),(b,count)): ((p,b),i))
 
     #combine with itself to generate pairs. Transforms the basket into tuples that ocur inside it
     #Result: ((partId, basketId), tuple([items]))
-    res[5] = expandedBaskets = expandedBaskets.join(expandedBaskets, numPartitions).filter(lambda ((p,b),(i1,i2)): i1[0]<i2[0]).mapValues(lambda (i1,i2): i1+i2)
+    res[5] = expandedBaskets = expandedBaskets.join(expandedBaskets).filter(lambda ((p,b),(i1,i2)): i1[0]<i2[0]).mapValues(lambda (i1,i2): i1+i2)
 
     #classic word count
     #Result: ((partId, tuple([items])), count)
-    res[6] = candidates_n = expandedBaskets.map(lambda ((p, b), items): ((p, items), 1), True).reduceByKey(lambda v1, v2: v1+v2)
+    res[6] = candidates_n = expandedBaskets.map(lambda ((p, b), items): ((p, items), 1)).reduceByKey(lambda v1, v2: v1+v2)
 
     #Determine frequent pairs by counting all pairs and filtering with threshold
     #Result: ((partId, tuple([items])), count)
@@ -70,14 +70,19 @@ def findFrequentItemsets(input, output, numPartitions, s, sc):
 
     #remove unfrequent pairs from baskets
     #Result: ((partId, basketId), tuple([items]))
-    res[8] = expandedBaskets = expandedBaskets.map(lambda ((p,b), i): ((p,i),b), True).leftOuterJoin(frequent_n, numPartitions).filter(lambda ((p, i), (b,count)): count!=None).map(lambda ((p,i),(b,count)): ((p,b),i), True)
-    """
-    #combine with itself to generate triplets
-    tuplesInBaskets = expandedBaskets.join(expandedBaskets).filter(lambda ((p,b),(t1,t2)): t1[0]<t2[0]).mapValues(lambda (i1, i2): sorted(i1+i2)).map(lambda ((p, b), items): ((p, items), b))
+    res[8] = expandedBaskets = expandedBaskets.map(lambda ((p,b), i): ((p,i),b)).join(frequent_n).map(lambda ((p,i),(b,count)): ((p,b),i))
 
-    #freqPairs = tuplesInBaskets.mapValues(lambda x: 1).reduceByKey(lambda v1, v2: v1+v2).filter(lambda c: c[1]>=threshold/numPartitions)
-    """
-    return res
+    #combine with itself to generate triplets
+    #TODO: cogroup instead
+    curr_n = 3
+    res[9] = expandedBaskets = expandedBaskets.join(expandedBaskets).filter(lambda ((p,b),(t1,t2)): t1[0]<t2[0] and t1[1]<t2[1]).mapValues(lambda (i1, i2): tuple(sorted(set(i1+i2)))).filter(lambda (k,v): len(set(v)) == curr_n)
+
+    #classic word count
+    res[10] = candidates_n = expandedBaskets.map(lambda ((p, b), items): ((p, items), 1)).reduceByKey(lambda v1, v2: v1+v2)
+
+    res[11] = frequent_n = candidates_n.filter(lambda c: c[1]>=threshold/numPartitions)
+
+    return res, threshold/numPartitions
 
 def getBreakUp():
     class nonlocal:
