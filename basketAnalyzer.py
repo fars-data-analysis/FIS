4861bf82dfa2a7737372f7df3fcc1ba2d568206c
djbb7 <daniel.bruzual@gmail.com>
2016-01-30 17:00:21 +0100

    Add script to analyze basket length and item freq

--- basketAnalyzer.py
+++ basketAnalyzer.py
@@ -14,17 +14,26 @@ if __name__ == "__main__":
     f_input = sys.argv[1]
     f_output = sys.argv[2]
 
-    baskets = sc.textFile(input).persist()
+    baskets = sc.textFile(f_input).map(lambda l: [int(x) for x in l.strip().split(' ')]).persist()
 
     # Number of baskets
     num_baskets = baskets.count()
 
     # Avg basket length
-    remapped = baskets.map(lambda k: len(k))).persist()
-    avg_basket_length = remapped.sum()/num_baskets
+    remapped = baskets.map(lambda k: len(k)).persist()
+    avg_basket_length = remapped.mean()
+
+    length_stdev = remapped.sampleStdev()
+    length_var   = remapped.sampleVariance()
 
     # Median basket length
-    med_basket_length = remapped.mean()
+    #Slower than selection
+    sorted_len = remapped.sortBy(lambda x: x)
+    collected_len = sorted_len.collect()
+    if len(collected_len) % 2 == 0:
+        med_basket_length = (collected_len[len(collected_len)/2-1] + collected_len[len(collected_len)/2]) /2
+    else:
+        med_basket_length = collected_len[len(collected_len)/2]
 
     # Number of different items
     all_items = baskets.flatMap(lambda b: [(x,1) for x in b]).persist()
@@ -32,24 +41,36 @@ if __name__ == "__main__":
     num_items = freq_items.count()
 
     # Avg item frequency
-    remapped = freq_items.map(lambda (k,v): k).persist()
-    avg_freq = remapped.sum()/num_items
+    remapped = freq_items.map(lambda (item,freq): freq).persist()
+    avg_freq = remapped.mean()
+
+    freq_stdev =  remapped.sampleStdev()
+    freq_var   = remapped.sampleVariance()
 
     # Median item frequency
-    med_freq = remapped.mean()
+    sorted_freq = remapped.sortBy(lambda x: x)
+    collected_freq = sorted_freq.collect()
+    if len(collected_freq) % 2 == 0:
+        med_freq = (collected_freq[len(collected_freq)/2 -1]+collected_freq[len(collected_freq)/2])/2
+    else:
+        med_freq = collected_freq[len(collected_freq)/2]
 
     fout = open(f_output, 'w')
 
     fout.write("Datafile: "+f_input+"\n")
 
-    fout.write("Num baskets: "+num_baskets+"\n")
+    fout.write("Num baskets: "+str(num_baskets)+"\n")
 
-    fout.write("Avg basket length: "+avg_basket_length+"\n")
+    fout.write("Avg basket length: "+str(avg_basket_length)+"\n")
 
-    fout.write("Median basket length: "+med_basket_length+"\n")
+    fout.write("Median basket length: "+str(med_basket_length)+"\n")
 
-    fout.write("Num items: "+num_items+"\n")
+    fout.write("Std. dev.: "+str(length_stdev)+", var.: "+str(length_var)+"\n")
 
-    fout.write("Avg item frequency: "+avg_freq+"\n")
+    fout.write("Num items: "+str(num_items)+"\n")
 
-    fout.write("Median item frequency: "+med_freq+"\n")
+    fout.write("Avg item frequency: "+str(avg_freq)+"\n")
+
+    fout.write("Median item frequency: "+str(med_freq)+"\n")
+
+    fout.write("Std. dev.: "+str(freq_stdev)+", var.: "+str(freq_var)+"\n")
