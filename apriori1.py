9572d056b5381c6c7c977e528511acbee23acb7b
sn1p3r46 <kingokongo46@gmail.com>
2016-01-30 17:05:41 +0100

    commented apriori1.py

--- apriori1.py
+++ apriori1.py
@@ -2,14 +2,25 @@ from collections import defaultdict
 from itertools import imap, combinations
 
 
+
+
+# input:
+#    data: List of lists containing numeric values
+#     min_sup: Minimum support ti be considered frequent (we select values that appears at least many times as s in baskets)
+#	  		steps: Number of steps in case we would like to find couples takes 2 (default 0 terminates when there are no more candidates to compute)
+#  returns:
+#	  solution: dictionary of sets of frozensets representing frequent itemsets, contains both numeric values and tuples.
+#		(be carefull on iterating on it)
 def get_frequent_items_sets(data,min_sup,steps=0):
+
 	# we transform the dataset in a list of sets
 	transactions = list()
+
 	# Temporary dictionary to count occurrences
 	items = defaultdict(lambda: 0)
+
 	# Returned dictionary
 	solution = dict()
-
 	L_set = set()
 
 	# Fills transactions and counts "singletons"
@@ -24,6 +35,8 @@ def get_frequent_items_sets(data,min_sup,steps=0):
 		if count >= min_sup:
 			L_set.add(frozenset([item]))
 
+	# Generalize the steps it ends when there are no candidates
+	# or if the user provided as input the number of parameters
 	k = 2
 	solution[k-1] = L_set
 	while L_set != set([]) and k != steps+1:
@@ -34,11 +47,29 @@ def get_frequent_items_sets(data,min_sup,steps=0):
 		k = k + 1
 	return solution
 
+
+
+
+
 # Creates candidates joining the same set
+# input:
+#    itemSet: a set of frequent items
+#     length: the cardinality of generated combinations
+#  returns:
+#	   set: a set containing all combinations with cardinality equal to length
+#		(be carefull on iterating on it)
 def create_candidates(itemSet, length):
         return set([i.union(j) for i in itemSet for j in itemSet if len(i.union(j)) == length])
 
-# Checks occurrencies and returns frequent items
+
+
+
+# Checks occurrences of items in transactions and returns frequent items
+# input:
+#    items: a set of frequent items (candidates)
+#     transactions: list of sets representing baskets
+#  returns:
+#	   _itemSet: a set containing all frequent candidates (a subset of inputs)
 def frequent_items(items, transactions, min_sup):
 	_itemSet = set()
 	counter = defaultdict(int)
